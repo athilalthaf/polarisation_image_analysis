0d37972a6ff49f78184719c09dd1b06db09a5443
Athil Althaf <athilalthafaz0@gmail.com>
2022-11-14 10:45:13 +0100

    Corrected for zenith luminance equation

--- Rayleigh_sky_model.py
+++ Rayleigh_sky_model.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 # import matplotlib.cm as cm
-gamma = np.linspace(0, 2 * np.pi, 200) # scattering angle
+gamma = np.linspace(0.00001, 2 * np.pi, 200) # scattering angle
 gamma_ang = np.linspace(0, 360, 200, dtype=int)
 Degree_of_pol = np.sin(gamma) ** 2 / (1 + (np.cos(gamma)) ** 2)
 
 Turbidity = 1
 
 theta_sun = np.deg2rad(60)
-gamma_sun = np.deg2rad(0)
+gamma_sun = np.deg2rad(0.00001)
 
 Y_dist_values = np.array([[0.1787,-1.4630],[-0.3554,0.4275],[-0.0227, 5.3251],[0.1206, -2.5771],[-0.0670, 0.3703]])
 Y_dist = np.matmul(Y_dist_values,np.matrix([Turbidity,1]).T)
@@ -42,20 +42,22 @@ def skylight_intensity(gamma,theta):
 def Polarisation(gamma,theta):
     return 1/C * lin_pol(gamma) * (theta * np.cos(theta) + (np.pi/2 - theta) * skylight_intensity(gamma, theta))
 
-
+sun_theta = 0.001
+sun_gamma = 0
+sun_phi = np.pi/3
 gamma_range = np.deg2rad(np.linspace(0, 360, 200))
-theta_range = np.deg2rad(np.linspace(90, 0, 100))
+theta_range = np.deg2rad(np.linspace(0.0001, 90, 100))
 
 gamma_mesh, theta_mesh = np.meshgrid(gamma_range, theta_range)
 
 Perez_test = Perez_luminance(gamma_mesh, theta_mesh)
 
-Y_z = Perez_luminance(0,0)
+Chi = (4/9 - Turbidity/120) * (np.pi - 2 * theta_sun)
+
+Y_z = (4.0453 * Turbidity - 4.9710) * np.tan(Chi) - 0.2155 * Turbidity + 2.4192
 Y = Y_z * Perez_luminance(gamma_mesh, theta_mesh)/Perez_luminance(0, theta_sun)
 
-sun_theta = np.pi/3
-sun_gamma = 0
-sun_phi = np.pi/3
+
 # plt.subplot(111)
 # plt.plot(gamma, Perez_luminance(theta=np.pi/2, gamma=gamma))
 # plt.xlabel("scattering angle")
@@ -63,23 +65,56 @@ sun_phi = np.pi/3
 # plt.title("Scattering angle v/s Luminance")
 # plt.show()
 
-# fig, ax = plt.subplots(dpi=120,subplot_kw=dict(projection="polar"))
-# ax.contourf(gamma_mesh, theta_mesh, Perez_test, 100,cmap="inferno")
-# plt.title("Perez Luminance based on the scatter angle and elevation")
-#
-# ax.set_yticks([np.deg2rad(30), np.deg2rad(60)])
-# ax.yaxis.set_ticklabels([30, 60])
-# # plt.ylim([90, 0])
-# plt.show()
+fig, ax = plt.subplots(dpi=120,subplot_kw=dict(projection="polar"),constrained_layout=True)
+plt1 = ax.contourf(gamma_mesh, theta_mesh, Perez_test, 100, cmap="inferno")
+plt.title("Perez Luminance based on the scatter angle and elevation")
+fig.colorbar(plt1)
+ax.set_yticks([np.deg2rad(30), np.deg2rad(60)])
+ax.yaxis.set_ticklabels([30, 60])
+# plt.ylim([90, 0])
+plt.show()
 
-fig2, ax2 = plt.subplots(dpi=120,subplot_kw=dict(projection="polar"))
-ax2.contourf(gamma_mesh, theta_mesh, Y, 100, cmap="inferno")
-plt.title("Perez Luminance based on the scatter angle and elevation including sun_position")
-ax2.scatter(sun_gamma,sun_theta)
+fig2, ax2 = plt.subplots(dpi=120, subplot_kw=dict(projection="polar"), constrained_layout=True)
+plt2 = ax2.contourf(gamma_mesh, theta_mesh, Y, 100, cmap="inferno")
+plt.title("Luminance Based on Zenith luminance")
+# ax2.scatter(sun_gamma, sun_theta)
+fig2.colorbar(plt2)
 ax2.set_yticks([np.deg2rad(30), np.deg2rad(60)])
 ax2.yaxis.set_ticklabels([30, 60])
 # plt.ylim([90, 0])
 plt.show()
+
+#
+# pol = Polarisation(gamma_mesh,theta_mesh)
+# fig3, ax3 = plt.subplots(dpi=120,subplot_kw=dict(projection="polar"))
+# ax2.contourf(gamma_mesh, theta_mesh, pol, 100, cmap="inferno")
+# plt.title("Polarisation pattern based on the scatter angle and elevation including sun_position")
+# # ax2.scatter(sun_gamma, sun_theta)
+# # ax2.set_yticks([np.deg2rad(30), np.deg2rad(60)])
+# # ax2.yaxis.set_ticklabels([30, 60])
+
+plt.show()
+
+fig4, ax4 = plt.subplots(dpi=120,subplot_kw=dict(projection="polar"),constrained_layout=True)
+plt4 = ax4.contourf(gamma_mesh, theta_mesh, Polarisation(gamma_mesh, theta_mesh), 100, cmap="inferno")
+plt.title("Polarisation pattern based on the scatter angle and elevation")
+fig4.colorbar(plt4)
+ax4.set_yticks([np.deg2rad(30), np.deg2rad(60)])
+ax4.yaxis.set_ticklabels([30, 60])
+# plt.ylim([90, 0])
+plt.show()
+
+fig5, ax5 = plt.subplots(dpi=120, constrained_layout=True)
+plt4 = ax4.plot(gamma_mesh, lin_pol(gamma_mesh))
+plt.title("Polarisation pattern based on the scatter angle and elevation")
+# ax4.set_xticks([0, np.deg2rad(90), np.deg2rad(180), np.deg2rad(360)])
+# tick = np.arange(0,361,90)
+# ax4.set_xticks(np.deg2rad(tick))
+#
+# ax4.xaxis.set_ticklabels(tick)
+# plt.ylim([90, 0])
+plt.show()
+
 
 # plt.subplot(111)
 # plt.plot(gamma, Degree_of_pol)
@@ -94,4 +129,3 @@ plt.show()
 # plt.ylabel("degree of polarisation")
 # plt.title("Scattering angle v/s degree of polarisation")
 # plt.show()
-
