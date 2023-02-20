0b13f97b8e8f09a64a2d9af31eb37dc8b56cab97
Athil Althaf <athilalthafaz0@gmail.com>
2023-02-20 23:11:20 +0100

    uncomment

--- plots_examples.py
+++ plots_examples.py
@@ -3,8 +3,6 @@
 Plotting different functions in pol_img_functions.
 separated by sections.
 
-uncomment sections to check different functions and its plots.
-
 loads different test images from test_images directory
 """
 import scipy.signal
@@ -59,168 +57,168 @@ equi_plot_after = pol_2_equirect(src=mapped_img, radius=radius, centre=centre,in
                     input image vis || plot the loaded image
 
 """
-#
-# fig1, ax1 = plt.subplots()
-#
-# ax1.set_title("Image with distortion")
-# # pixel_map= pixel_map_func(calib_img, radius = 258, elevation_map_src=elevation_map_src, elevation_map_corr=elevation_map_corr, azimuth_map=azi_map)
-# #ele_range = np.deg2rad(np.arange(0, 91, 30))
-# plt.imshow(blend_img_med)
-# # cbar = fig13.colorbar(mat10, ax=ax13, ticks=ele_range, label ="angles")
-# # cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
-# plt.xlabel("pixels")
-# plt.ylabel("pixels")
-# plt.tight_layout()
-# # plt.imsave("calib_img_subsampled.png",calib_img_subsampled )
+
+fig1, ax1 = plt.subplots()
+
+ax1.set_title("Image with distortion")
+# pixel_map= pixel_map_func(calib_img, radius = 258, elevation_map_src=elevation_map_src, elevation_map_corr=elevation_map_corr, azimuth_map=azi_map)
+#ele_range = np.deg2rad(np.arange(0, 91, 30))
+plt.imshow(blend_img_low)
+# cbar = fig13.colorbar(mat10, ax=ax13, ticks=ele_range, label ="angles")
+# cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
+plt.xlabel("pixels")
+plt.ylabel("pixels")
+plt.tight_layout()
+# plt.imsave("calib_img_subsampled.png",calib_img_subsampled )
 # plt.savefig("blend_med_fig.png", dpi = 300,bbox_inches = "tight")
-# plt.show()
+plt.show()
 
 
 """
                     mapped image
 """
 
-# fig1, ax1 = plt.subplots()
-#
-# ax1.set_title("corrected image with pixel map function, azi_thresh:{}$^\circ$".format(thresh))
-# # pixel_map= pixel_map_func(calib_img, radius = 258, elevation_map_src=elevation_map_src, elevation_map_corr=elevation_map_corr, azimuth_map=azi_map)
-# ele_range = np.deg2rad(np.arange(0, 91, 30))
-# plt.imshow(mapped_img)
-# # cbar = fig13.colorbar(mat10, ax=ax13, ticks=ele_range, label ="angles")
-# # cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
-# plt.xlabel("pixels")
-# plt.ylabel("pixels")
-# plt.tight_layout()
-# # plt.imsave("calib_img_subsampled.png",calib_img_subsampled )
+fig2, ax2 = plt.subplots()
+
+ax2.set_title("corrected image with pixel map function, azi_thresh:{}$^\circ$".format(thresh))
+# pixel_map= pixel_map_func(calib_img, radius = 258, elevation_map_src=elevation_map_src, elevation_map_corr=elevation_map_corr, azimuth_map=azi_map)
+ele_range = np.deg2rad(np.arange(0, 91, 30))
+plt.imshow(mapped_img)
+# cbar = fig13.colorbar(mat10, ax=ax13, ticks=ele_range, label ="angles")
+# cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
+plt.xlabel("pixels")
+plt.ylabel("pixels")
+plt.tight_layout()
+# plt.imsave("calib_img_subsampled.png",calib_img_subsampled )
 # plt.savefig("blend_high_mapped.png", dpi = 300,bbox_inches = "tight")
 # plt.imsave("blend_high_mapped_im.png",mapped_img)
-# plt.show()
+plt.show()
 
 """
                     equirect projection
 """
 
-# fig1, ax1 = plt.subplots()
-#
-# ax1.set_title("Equirect projection of original image")
-# plt.imshow(equi_plot_after)
-# plt.xticks(np.linspace(0,int(2 * np.pi * radius),9))
-# ax1.set_xticklabels(np.linspace(0,360,9,dtype="int"))
-#
-# plt.yticks(np.linspace(0, radius, 4))
-# ax1.set_yticklabels(np.linspace(90, 0, 4,dtype="int"))
-#
-# xtick_levels =np.linspace(0,int(2 * np.pi * radius)-1,9,dtype=int)
-# ytick_levels =np.linspace(0,radius-1, 4,dtype=int)
-#
-# sample_img_med = [255, 255, 255] * np.ones(equi_plot_after.shape)
-# sample_img_med[ytick_levels, :] = [0, 0, 0]
-# sample_img_med[:, xtick_levels] = [0, 0, 0]
-#
-# # cbar = fig13.colorbar(mat10, ax=ax13, ticks=ele_range, label ="angles")
-# # cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
-# plt.xlabel("azimuth in degrees ($^\circ$)")
-# plt.ylabel("elevation in degrees ($^\circ$)")
-#
-# plt.tight_layout()
+fig3, ax3 = plt.subplots()
+
+ax3.set_title("Equirect projection of original image")
+plt.imshow(equi_plot_after)
+plt.xticks(np.linspace(0,int(2 * np.pi * radius),9))
+ax3.set_xticklabels(np.linspace(0,360,9,dtype="int"))
+
+plt.yticks(np.linspace(0, radius, 4))
+ax3.set_yticklabels(np.linspace(90, 0, 4,dtype="int"))
+
+xtick_levels =np.linspace(0,int(2 * np.pi * radius)-1,9,dtype=int)
+ytick_levels =np.linspace(0,radius-1, 4,dtype=int)
+
+sample_img_med = [255, 255, 255] * np.ones(equi_plot_after.shape)
+sample_img_med[ytick_levels, :] = [0, 0, 0]
+sample_img_med[:, xtick_levels] = [0, 0, 0]
+
+# cbar = fig13.colorbar(mat10, ax=ax13, ticks=ele_range, label ="angles")
+# cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
+plt.xlabel("azimuth in degrees ($^\circ$)")
+plt.ylabel("elevation in degrees ($^\circ$)")
+
+plt.tight_layout()
 # plt.imsave("equirect_test_mid_res_im.png",equi_plot_after )
-# ax1.set_aspect(4)
+ax3.set_aspect(4)
 # plt.savefig("aurora_equi_before_800.png", dpi = 300, bbox_inches="tight")
-# plt.show()
+plt.show()
 
 """
                     validation image
 """
 
-# fig1, ax1 = plt.subplots()
-#
-# ytick_levels =np.linspace(0,equi_plot_after.shape[0], 4,dtype=int)
-# ytick_levels[-1] = ytick_levels[-1] - 1
-# xtick_levels =np.linspace(0,equi_plot_after.shape[1] -1,9,dtype=int)
-# xtick_levels[-1] = xtick_levels[-1] - 1
-#
-# sample_img_med = [0, 0, 0] * np.ones(equi_plot_after.shape)
-# sample_img_med[ytick_levels, :] = [255, 255, 255]
-# sample_img_med[:, xtick_levels] = [255, 255, 255]
-#
-#
-# ax1.set_title("caliberated image expected longitude and latitutes ")
-# plt.imshow(sample_img_med)
-# # plt.imshow(sample_img_med,alpha=0.5)
-# plt.xticks(np.linspace(0,int(2 * np.pi * radius),9))
-# ax1.set_xticklabels(np.linspace(0, 360, 9, dtype="int"))
-#
-# plt.yticks(np.linspace(0, radius, 4))
-# ax1.set_yticklabels(np.linspace(90, 0, 4,dtype="int"))
-#
-#
-# # cbar = fig13.colorbar(mat10, ax=ax13, ticks=ele_range, label ="angles")
-# # cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
-# plt.xlabel("azimuth in degrees ($^\circ$)")
-# plt.ylabel("elevation in degrees ($^\circ$)")
-#
-# plt.tight_layout()
-# # plt.imsave("calib_img_subsampled.png",calib_img_subsampled )
-# ax1.set_aspect(4)
+fig4, ax4 = plt.subplots()
+
+ytick_levels =np.linspace(0,equi_plot_after.shape[0], 4,dtype=int)
+ytick_levels[-1] = ytick_levels[-1] - 1
+xtick_levels =np.linspace(0,equi_plot_after.shape[1] -1,9,dtype=int)
+xtick_levels[-1] = xtick_levels[-1] - 1
+
+sample_img_med = [0, 0, 0] * np.ones(equi_plot_after.shape)
+sample_img_med[ytick_levels, :] = [255, 255, 255]
+sample_img_med[:, xtick_levels] = [255, 255, 255]
+
+
+ax4.set_title("caliberated image expected longitude and latitutes ")
+plt.imshow(sample_img_med)
+# plt.imshow(sample_img_med,alpha=0.5)
+plt.xticks(np.linspace(0,int(2 * np.pi * radius),9))
+ax4.set_xticklabels(np.linspace(0, 360, 9, dtype="int"))
+
+plt.yticks(np.linspace(0, radius, 4))
+ax4.set_yticklabels(np.linspace(90, 0, 4,dtype="int"))
+
+
+# cbar = fig13.colorbar(mat10, ax=ax13, ticks=ele_range, label ="angles")
+# cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
+plt.xlabel("azimuth in degrees ($^\circ$)")
+plt.ylabel("elevation in degrees ($^\circ$)")
+
+plt.tight_layout()
+# plt.imsave("calib_img_subsampled.png",calib_img_subsampled )
+ax4.set_aspect(4)
 # plt.savefig("valid_img_med_black.png", dpi = 300, bbox_inches="tight")
-# plt.show()
+plt.show()
 
 """
                     kernel visualisation 
 """
-# sigma = 5
-# kern_size = 4 * sigma + 1
-# ele_val = 0
-# kernel = gauss_kernel(sigma=sigma,ele_val=ele_val,kern_size_x=kern_size)
-#
-# fig1, ax1 = plt.subplots()
-#
-# ax1.set_title("simple gaussian kernel , elevation :{}$^\circ$".format(ele_val))
-# plt1 = plt.imshow(kernel)
-# cbar = fig1.colorbar(plt1, ax=ax1, label ="weights")
-# # cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
-# plt.xlabel("pixel cordinates")
-# plt.ylabel("pixel cordinates")
-# plt.tight_layout()
-# # plt.imsave("calib_img_subsampled.png",calib_img_subsampled )
+sigma = 5
+kern_size = 4 * sigma + 1
+ele_val = 0
+kernel = gauss_kernel(sigma=sigma,ele_val=ele_val,kern_size_x=kern_size)
+
+fig5, ax5 = plt.subplots()
+
+ax5.set_title("simple gaussian kernel , elevation :{}$^\circ$".format(ele_val))
+plt5 = plt.imshow(kernel)
+cbar = fig1.colorbar(plt5, ax=ax1, label ="weights")
+# cbar.set_ticklabels(np.ceil(np.rad2deg(ele_range)))
+plt.xlabel("pixel cordinates")
+plt.ylabel("pixel cordinates")
+plt.tight_layout()
+# plt.imsave("calib_img_subsampled.png",calib_img_subsampled )
 # plt.savefig("simple_gauss_kernel_0.png", dpi = 300,bbox_inches = "tight")
-#
-# plt.show()
+
+plt.show()
 
 """
                     tile functions
 """
 
-# side_tile = equi_plot_before.shape[1]//8
-# top_tile = equi_plot_before.shape[0]//3
-# tiled_im = image_tile_function(equi_plot_after,side_tile,top_tile)
-#
-# fig1, ax1 = plt.subplots()
-# ax1.set_title("sample tiling function in action , side_tile:{}$^\circ$, top_tile :{}$^\circ$"
-#               .format(int(np.ceil(side_tile/equi_plot_after.shape[1]*360)), int(np.ceil(top_tile/equi_plot_after.shape[0]*90))))
-# azi_ticks = np.linspace(0, tiled_im.shape[1],11)
-# azi_ticks_labels = np.concatenate([[360-45],np.linspace(0,360,9,dtype=int),[45]])
-#
-# ele_ticks = np.linspace(0, tiled_im.shape[0],5)
-# ele_ticks_labels = np.concatenate([[60],np.linspace(0,90,4,dtype=int)[::-1]])
-#
-#
-# plt.xticks(azi_ticks)
-# ax1.set_xticklabels(azi_ticks_labels)
-# plt.yticks(ele_ticks)
-# ax1.set_yticklabels(ele_ticks_labels)
-#
-#
-# plt1 = plt.imshow(tiled_im)
-#
-#
-# plt.xlabel("azimuth in ($^\circ$)")
-# plt.ylabel("elevation in ($^\circ$)")
-# ax1.set_aspect(tiled_im.shape[1]/tiled_im.shape[0])
-# # plt.tight_layout()
+side_tile = equi_plot_before.shape[1]//8
+top_tile = equi_plot_before.shape[0]//3
+tiled_im = image_tile_function(equi_plot_after,side_tile,top_tile)
+
+fig6, ax6 = plt.subplots()
+ax6.set_title("sample tiling function in action , side_tile:{}$^\circ$, top_tile :{}$^\circ$"
+              .format(int(np.ceil(side_tile/equi_plot_after.shape[1]*360)), int(np.ceil(top_tile/equi_plot_after.shape[0]*90))))
+azi_ticks = np.linspace(0, tiled_im.shape[1],11)
+azi_ticks_labels = np.concatenate([[360-45],np.linspace(0,360,9,dtype=int),[45]])
+
+ele_ticks = np.linspace(0, tiled_im.shape[0],5)
+ele_ticks_labels = np.concatenate([[60],np.linspace(0,90,4,dtype=int)[::-1]])
+
+
+plt.xticks(azi_ticks)
+ax6.set_xticklabels(azi_ticks_labels)
+plt.yticks(ele_ticks)
+ax6.set_yticklabels(ele_ticks_labels)
+
+
+plt6 = plt.imshow(tiled_im)
+
+
+plt.xlabel("azimuth in ($^\circ$)")
+plt.ylabel("elevation in ($^\circ$)")
+ax6.set_aspect(tiled_im.shape[1]/tiled_im.shape[0])
+# plt.tight_layout()
 # plt.imsave("tiled_img_sample_im_01.png",tiled_im)
 # plt.savefig("tiled_img_sample_01.png", dpi = 300,bbox_inches = "tight")
-# plt.show()
+plt.show()
 
 """
                     test image for convolution
